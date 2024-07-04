c62653e4175c77a56f6768756bbd10497e34d0eb
Christopher Fordant <christopherfordant1@gmail.com>
2024-07-04 10:24:08 +0200

    fin de projet

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var sp, btn_start, btn_stop, t, ms, s, mn, h;
 //fonctions pour initialiser les variables quand la page se charge
-window.onload(){
+window.onload = function () {
     sp = document.getElementsByTagName('span');
     btn_start = document.getElementById('start');
     btn_stop = document.getElementById('stop');
@@ -13,15 +13,15 @@ window.onload(){
 function update_chrono() {
     ms += 1;
     if (ms == 10) {
-        ms == 1;
+        ms = 1;
         s += 1;
     }
     if (s == 60) {
-        s == 0;
+        s = 0;
         mn += 1
     }
     if (mn == 60) {
-        mn == 0;
+        mn = 0;
         h += 1
     }
     //insertions des valeurs dans les spans 
@@ -34,6 +34,25 @@ function update_chrono() {
 
 //mettre en place la fonction du bouton start
 function start() {
-    t = setInterval(update_chrono(), 100);
+    t = setInterval(update_chrono, 100);
     btn_start.disabled = true;
-}
+};
+
+function stop() {
+    clearInterval(t);
+    btn_start.disabled = false
+};
+
+function reset() {
+    clearInterval(t);
+    ms = 0, s = 0, mn = 0, h = 0;
+    sp[0].innerHTML = h + "h";
+    sp[1].innerHTML = mn + "min";
+    sp[2].innerHTML = s + "s";
+    sp[3].innerHTML = ms + "ms";
+    btn_start.disabled = false;
+};
+
+
+
+
